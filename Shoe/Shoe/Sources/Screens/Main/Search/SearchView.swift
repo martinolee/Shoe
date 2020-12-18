b9077eb6e456d9c3fde451937e70c271e6e0bb31
martinolee <soohan.m.lee@gmail.com>
2020-12-19 00:33:23 +0900

    add SearchView

---
+++ Shoe/Shoe/Sources/Screens/Main/Search/SearchView.swift
@@ -0,0 +1,24 @@
+//
+//  SearchView.swift
+//  Shoe
+//
+//  Created by Soohan Lee on 2020/12/19.
+//
+
+import SwiftUI
+
+
+struct SearchView: View {
+  var body: some View {
+    Text("Search View")
+  }
+}
+
+
+// MARK: - Preview
+
+struct SearchView_Previews: PreviewProvider {
+  static var previews: some View {
+    SearchView()
+  }
+}
