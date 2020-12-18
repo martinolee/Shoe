31912b0198121bc52be0f45cf276cee0a54b43d7
martinolee <soohan.m.lee@gmail.com>
2020-12-19 00:22:17 +0900

    update file hierarchy

---
+++ Shoe/Shoe/Sources/App/ShoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeApp.swift
+//  Shoe
+//
+//  Created by Soohan Lee on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct ShoeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello, World!")
+    }
+  }
+}
