28a917f4fffa973776bac225f4f69157ddaa9869
martinolee <soohan.m.lee@gmail.com>
2020-12-19 00:23:24 +0900

    add MainView

--- Shoe/Shoe/Sources/App/ShoeApp.swift
+++ Shoe/Shoe/Sources/App/ShoeApp.swift
@@ -7,11 +7,12 @@
 
 import SwiftUI
 
+
 @main
 struct ShoeApp: App {
   var body: some Scene {
     WindowGroup {
-      Text("Hello, World!")
+      MainView()
     }
   }
 }
---
+++ Shoe/Shoe/Sources/Screens/Main/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  Shoe
+//
+//  Created by Soohan Lee on 2020/12/19.
+//
+
+import SwiftUI
+
+
+struct MainView: View {
+  var body: some View {
+    Text("Main View")
+  }
+}
+
+
+// MARK: - Preview
+
+struct MainView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainView()
+  }
+}
