1ab92cab2beca40883b67f20d366c3934ed60a4c
martinolee <soohan.m.lee@gmail.com>
2020-12-19 00:32:04 +0900

    add HomeView

---
+++ Shoe/Shoe/Sources/Screens/Main/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Shoe
+//
+//  Created by Soohan Lee on 2020/12/19.
+//
+
+import SwiftUI
+
+
+struct HomeView: View {
+  var body: some View {
+    Text("Home View")
+  }
+}
+
+
+// MARK: - Preview
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
