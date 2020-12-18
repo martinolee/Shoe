94dccb053afb51bdf0aa13d6b92313d27a55c11b
martinolee <soohan.m.lee@gmail.com>
2020-12-19 00:36:08 +0900

    add CalendarView

---
+++ Shoe/Shoe/Sources/Screens/Main/Calendar/CalendarView.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarView.swift
+//  Shoe
+//
+//  Created by Soohan Lee on 2020/12/19.
+//
+
+import SwiftUI
+
+
+struct CalendarView: View {
+  var body: some View {
+    Text("Calendar View")
+  }
+}
+
+
+// MARK: - Preview
+
+struct CalendarView_Previews: PreviewProvider {
+  static var previews: some View {
+    CalendarView()
+  }
+}
